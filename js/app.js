98b89d795f0d6f47de68a470a1f057cd598e6641
Hannah Nordgren <hey@hereshannahs.info>
2014-06-06 16:20:19 -0400

    trying yield

--- js/app.js
+++ js/app.js
@@ -2,10 +2,17 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   this.resource('index', {path: '/'});
+  this.resource('my_layout');
   this.resource('about');
   this.resource('surprise');
   this.resource('richland');
 });
+
+AViewWithLayout = Ember.View.extend({
+  layoutName: 'my_layout',
+  templateName: 'about'
+});
+
 
 App.RichlandRoute = Ember.Route.extend({
   model: function(){
