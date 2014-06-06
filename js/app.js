a0dfb1d6027146307c30a43eb9f0932b2aaf7226
Hannah Nordgren <hey@hereshannahs.info>
2014-06-06 10:12:18 -0400

    set up project with ember, added compass and bootstrap in compass

---
+++ js/app.js
@@ -0,0 +1,12 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.resource('a', {path: '/a'});
+  this.resource('hello', {path: '/hello'});
+});
+
+// App.IndexRoute = Ember.Route.extend({
+//   model: function() {
+//     return ['red', 'yellow', 'blue'];
+//   }
+// });
